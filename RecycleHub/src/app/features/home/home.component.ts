9caa144e467c4f8bf1f8fa2571c8709a2b17dcfd
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2025-02-04 13:54:15 +0100

    added header and footer in the home page

--- RecycleHub/src/app/features/home/home.component.ts
+++ RecycleHub/src/app/features/home/home.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import {FooterComponent} from '../../shared/footer/footer.component';
+import {HeaderComponent} from '../../shared/header/header.component';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [
+    FooterComponent,
+    HeaderComponent
+  ],
   templateUrl: './home.component.html',
   standalone: true,
   styleUrl: './home.component.css'
