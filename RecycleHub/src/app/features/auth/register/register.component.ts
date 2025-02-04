95ad3bb712720047fe0962623e3eddb0419f0629
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2025-02-04 11:37:13 +0100

    added register component

---
+++ RecycleHub/src/app/features/auth/register/register.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  standalone: true,
+  selector: 'app-register',
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.css'
+})
+export class RegisterComponent {
+  registerForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.registerForm = this.fb.group({
+      username: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', Validators.required]
+    }, { validator: this.passwordMatchValidator });
+  }
+
+  passwordMatchValidator(form: FormGroup) {
+    return form.get('password')!.value === form.get('confirmPassword')!.value
+      ? null : { mismatch: true };
+  }
+
+  onSubmit() {
+    if (this.registerForm.valid) {
+      console.log('Formulaire soumis :', this.registerForm.value);
+      // Appel au service d'inscription ici
+    } else {
+      console.log('Formulaire invalide');
+    }
+  }
+}
