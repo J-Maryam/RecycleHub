a50008ccd2b8e5d0f67b7bd92a1627f119e4c23a
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2025-02-04 11:14:00 +0100

    added header of the homepage

--- RecycleHub/src/app/app.component.ts
+++ RecycleHub/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {HomeComponent} from './features/home/home.component';
+import {HeaderComponent} from './shared/header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HomeComponent],
+  imports: [RouterOutlet, HomeComponent, HeaderComponent],
   templateUrl: './app.component.html',
   standalone: true,
   styleUrl: './app.component.css'
