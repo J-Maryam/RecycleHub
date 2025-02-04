5a6d067a2301edc28f4cd60a6167262c0dc174d9
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2025-02-04 11:17:13 +0100

    added footer of the homepage

--- RecycleHub/src/app/app.component.ts
+++ RecycleHub/src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {HomeComponent} from './features/home/home.component';
 import {HeaderComponent} from './shared/header/header.component';
+import {FooterComponent} from './shared/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HomeComponent, HeaderComponent],
+  imports: [RouterOutlet, HomeComponent, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   standalone: true,
   styleUrl: './app.component.css'
