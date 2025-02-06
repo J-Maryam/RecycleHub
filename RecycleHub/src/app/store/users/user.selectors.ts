7bf0e4c01819d345acf611d5c9dec7ff09bb0fd5
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2025-02-06 16:55:39 +0100

    ..

--- RecycleHub/src/app/store/users/user.selectors.ts
+++
@@ -1,5 +0,0 @@
-import { createSelector } from '@ngrx/store';
-import { UserState } from './user.state';
-
-export const selectUserState = (state: { users: UserState }) => state.users;
-export const selectAllUsers = createSelector(selectUserState, (state) => state.users);
