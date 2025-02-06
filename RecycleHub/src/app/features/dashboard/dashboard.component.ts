be20f89aa376d2856db8f06ea856060031ed57c8
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2025-02-06 23:58:46 +0100

    feat: added dashboard and sidebar components

--- RecycleHub/src/app/features/dashboard/dashboard.component.ts
+++ RecycleHub/src/app/features/dashboard/dashboard.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../../core/services/auth.service';
+import {SidebarComponent} from '../../shared/components/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   standalone: true,
+  imports: [
+    SidebarComponent
+  ],
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent {
@@ -13,6 +17,14 @@ export class DashboardComponent {
   constructor(private authService: AuthService) {
     this.userName = localStorage.getItem('userName') || 'Utilisateur';
   }
+
+  stats = {
+    enAttente: 5,
+    enCours: 2,
+    valides: 10,
+    rejetes: 3,
+    points: 250
+  };
 
   logout(): void {
     this.authService.logout();
