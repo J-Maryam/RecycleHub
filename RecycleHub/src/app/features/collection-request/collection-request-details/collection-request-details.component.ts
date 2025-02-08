3658a79b78cb6e7c74e7e0b26d027b90f55485aa
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2025-02-08 19:35:21 +0100

    feat: added popup to display the details of the collection requests

--- RecycleHub/src/app/features/collection-request/collection-request-details/collection-request-details.component.ts
+++ RecycleHub/src/app/features/collection-request/collection-request-details/collection-request-details.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import {CollectionRequest} from '../../../shared/models/collection-request.model';
+import {DatePipe, NgClass} from '@angular/common';
 
 @Component({
   selector: 'app-collection-request-details',
-  imports: [],
   templateUrl: './collection-request-details.component.html',
-  styleUrl: './collection-request-details.component.css'
+  standalone: true,
+  imports: [
+    NgClass,
+    DatePipe
+  ],
+  styleUrls: ['./collection-request-details.component.css']
 })
 export class CollectionRequestDetailsComponent {
+  @Input() request!: CollectionRequest;
+  @Output() close = new EventEmitter<void>();
 
+  closeModal() {
+    this.close.emit(); // Émet un événement pour fermer la modale
+  }
 }
--- RecycleHub/src/app/features/collection-request/collection-request-list/collection-request-list.component.ts
+++ RecycleHub/src/app/features/collection-request/collection-request-list/collection-request-list.component.ts
@@ -1,19 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-import { DatePipe, NgClass, NgForOf } from '@angular/common';
+import {DatePipe, NgClass, NgForOf, NgIf} from '@angular/common';
 import { SidebarComponent } from '../../../shared/components/sidebar/sidebar.component';
 import { NavbarComponent } from '../../../shared/components/navbar/navbar.component';
 import {CollectionRequest} from '../../../shared/models/collection-request.model';
 import {CollectionService} from '../../../core/services/collection-request.service';
+import {CollectionRequestDetailsComponent} from '../collection-request-details/collection-request-details.component';
 
 @Component({
   selector: 'app-collection-request-list',
   templateUrl: './collection-request-list.component.html',
   standalone: true,
-  imports: [DatePipe, NgForOf, SidebarComponent, NavbarComponent, NgClass],
+  imports: [DatePipe, NgForOf, SidebarComponent, NavbarComponent, NgClass, NgIf, CollectionRequestDetailsComponent],
   styleUrls: ['./collection-request-list.component.css']
 })
 export class CollectionRequestListComponent implements OnInit {
   requests: CollectionRequest[] = [];
+  selectedRequest: CollectionRequest | null = null;
 
   constructor(private collectionService: CollectionService) {}
 
@@ -23,27 +25,9 @@ export class CollectionRequestListComponent implements OnInit {
   }
 
   showDetails(request: CollectionRequest) {
-    alert(`Détails de la collecte:\n
-    - Date : ${request.preferredDate}
-    - Type : ${request.wasteTypes.join(', ')}
-    - Poids : ${request.estimatedWeight / 1000} kg
-    - Utilisateur : ${request.userId}
-    - Adresse : ${request.collectionAddress}
-    - Statut : ${request.status}`);
+    this.selectedRequest = request;
   }
-
-  // editRequest(request: CollectionRequest) {
-  //   console.log('Modifier la requête:', request);
-  //   // Naviguer vers un formulaire d'édition avec l'ID de la requête
-  //   this.router.navigate(['/edit-collection', request.id]);
-  // }
-  //
-  // deleteRequest(requestId: number) {
-  //   if (confirm('Êtes-vous sûr de vouloir supprimer cette requête ?')) {
-  //     this.collectionRequestService.deleteRequest(requestId).subscribe(() => {
-  //       this.requests = this.requests.filter(req => req.id !== requestId);
-  //     });
-  //   }
-  // }
-
+  closeDetails() {
+    this.selectedRequest = null;
+  }
 }
