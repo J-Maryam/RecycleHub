401fb131bf0e4a981281514c1fb17a4874023844
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2025-02-09 02:42:54 +0100

    refactor: update the style of the navbar

--- RecycleHub/src/app/shared/components/navbar/navbar.component.ts
+++ RecycleHub/src/app/shared/components/navbar/navbar.component.ts
@@ -20,7 +20,7 @@ export class NavbarComponent implements OnInit {
     role: ''
   };
 
-  constructor(private authService: AuthService) {}
+  // constructor(private authService: AuthService) {}
 
   ngOnInit(): void {
     const storedUser = localStorage.getItem('currentUser');
