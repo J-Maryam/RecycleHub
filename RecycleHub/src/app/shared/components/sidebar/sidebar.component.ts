01e60369c926c21463f0c7facdf110e63e882b91
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2025-02-09 17:28:17 +0100

    feat: added verification to sidebar: display the task associated to every user depends to the role

--- RecycleHub/src/app/shared/components/sidebar/sidebar.component.ts
+++ RecycleHub/src/app/shared/components/sidebar/sidebar.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Router, RouterLink, RouterLinkActive} from '@angular/router';
 import { AuthService } from '../../../core/services/auth.service';
+import {NgIf} from '@angular/common';
+import {CollectionService} from '../../../core/services/collection-request.service';
 
 @Component({
   selector: 'app-sidebar',
@@ -8,15 +10,28 @@ import { AuthService } from '../../../core/services/auth.service';
   standalone: true,
   imports: [
     RouterLink,
-    RouterLinkActive
+    RouterLinkActive,
+    NgIf
   ],
   styleUrls: ['./sidebar.component.css']
 })
-export class SidebarComponent {
+export class SidebarComponent implements OnInit{
   userName: string | null = '';
+  userRole: string = '';
 
-  constructor(private authService: AuthService, private router: Router) {
+  constructor(private authService: AuthService, private router: Router, private collectionService: CollectionService) {
     this.userName = localStorage.getItem('userName') || 'Utilisateur';
+  }
+
+  ngOnInit(): void {
+    this.setUserRole();
+  }
+
+  setUserRole() {
+    const currentUser = this.collectionService.getCurrentUser();
+    if (currentUser && currentUser.role) {
+      this.userRole = currentUser.role;
+    }
   }
 
   logout(): void {
