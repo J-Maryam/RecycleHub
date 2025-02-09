aae883a1b0d02980abdb733c07c9c6b97bb69fcc
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2025-02-09 03:19:16 +0100

    feat: added the user that he created the collection request in the details card

--- RecycleHub/src/app/features/collection-request/collection-request-details/collection-request-details.component.ts
+++ RecycleHub/src/app/features/collection-request/collection-request-details/collection-request-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import {CollectionRequest} from '../../../shared/models/collection-request.model';
 import {DatePipe, NgClass} from '@angular/common';
+import {CollectionService} from '../../../core/services/collection-request.service';
 
 @Component({
   selector: 'app-collection-request-details',
@@ -13,6 +14,10 @@ import {DatePipe, NgClass} from '@angular/common';
   styleUrls: ['./collection-request-details.component.css']
 })
 export class CollectionRequestDetailsComponent {
+
+  constructor(protected collectionService: CollectionService) {}
+
+
   @Input() request!: CollectionRequest;
   @Output() close = new EventEmitter<void>();
 
